6c11c5d1ecddf343c67885ac0d24384c628a2c27
Robert Siński <75808735+Biplo12@users.noreply.github.com>
2023-08-02 21:09:37 +0200

    Initial commit from Create Next App

---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'liberty-blue': '#0A192F',
+        spindrift: '#64FFDA',
+      },
+      fontSize: {
+        '4xl': '2.5rem',
+        '3xl': '1.875rem',
+        '2xl': '1.5rem',
+        xl: '1.25rem',
+        lg: '1.125rem',
+        base: '1rem',
+        sm: '0.875rem',
+        xs: '0.75rem',
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+} satisfies Config;
