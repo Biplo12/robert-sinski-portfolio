630910cb259d5b0b294d108fcc74667b7b3d7d76
Robert Siński <75808735+Biplo12@users.noreply.github.com>
2023-08-03 18:02:59 +0200

    feat: contact responsive

--- src/components/Contact/Partials/ContactHeader.tsx
+++ src/components/Contact/Partials/ContactHeader.tsx
@@ -6,7 +6,7 @@ const ContactHeader: React.FC = (): JSX.Element => {
       <h3 className='text-spindrift text-center text-2xl font-bold'>
         04. What's Next?
       </h3>
-      <h1 className='text-5xl font-bold'>Get In Touch</h1>
+      <h1 className='text-center text-5xl font-bold'>Get In Touch</h1>
       <p className='text-gray-text mt-4 max-w-[500px] text-center'>
         I am currently looking for any new opportunities, my inbox is always
         open. If you have a job offer or just want to say hi, feel free to
