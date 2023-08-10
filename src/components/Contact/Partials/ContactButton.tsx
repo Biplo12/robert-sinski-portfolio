d337bd2417f0ca9e1fb0000dae7e2d618e6c0b96
Robert Siński <75808735+Biplo12@users.noreply.github.com>
2023-08-10 11:11:05 +0200

    fix: button position

--- src/components/Contact/Partials/ContactButton.tsx
+++ src/components/Contact/Partials/ContactButton.tsx
@@ -6,7 +6,7 @@ const ContactButton: React.FC = (): JSX.Element => {
     <div className='relative z-20 flex flex-col items-start justify-start font-mono'>
       <a
         href={`mailto:${MAIL}`}
-        className='border-spindrift text-spindrift bg-liberty-blue relative z-20 w-[112px] rounded-sm border px-5 py-2 text-sm font-bold transition duration-150 ease-linear hover:translate-x-[-2px] hover:translate-y-[-2px] hover:transform'
+        className='border-spindrift text-spindrift bg-liberty-blue relative z-20 w-[112px] rounded-sm border px-5 py-2 text-center text-sm font-bold transition duration-150 ease-linear hover:translate-x-[-2px] hover:translate-y-[-2px] hover:transform'
       >
         Say Hello
       </a>
--- src/components/Hero/Partials/HeroButton.tsx
+++ src/components/Hero/Partials/HeroButton.tsx
@@ -16,7 +16,7 @@ const HeroButton: React.FC = (): JSX.Element => {
         href='/resume.pdf'
         target='_blank'
         rel='noopener noreferrer'
-        className='border-spindrift text-spindrift bg-liberty-blue relative z-20 mt-10 w-[200px] rounded-sm border px-5 py-3.5 text-sm font-bold transition duration-150 ease-linear hover:translate-x-[-2.5px] hover:translate-y-[-5px] hover:transform'
+        className='border-spindrift text-spindrift bg-liberty-blue relative z-20 mt-10 w-[200px] rounded-sm border px-5 py-3.5 text-center text-sm font-bold transition duration-150 ease-linear hover:translate-x-[-2.5px] hover:translate-y-[-5px] hover:transform'
       >
         Check out my resume!
       </a>
